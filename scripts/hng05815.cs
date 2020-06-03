98edeb1eb3eec04cb2374468ddc845cd2c52d597
nuel-clet <59366858+nuel-clet@users.noreply.github.com>
2020-06-03 04:44:47 +0100

    Create hng05815.cs
    Task-2: A formal introduction with details.

---
+++ scripts/hng05815.cs
@@ -0,0 +1,8 @@
+   static void Main(string[] args)
+        {
+            int id =5815;
+            string name = "Ikenna Ezerebo";
+            string language = "C#"; 
+                
+            Console.WriteLine("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage task", name, id, language);
+        }
